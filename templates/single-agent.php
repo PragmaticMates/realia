a2f4ad869ecf9d2741cfec1a20a9093b9eaf1429
Lukas <lukas.vinclav@gmail.com>
2015-05-29 11:43:03 +0200

    extra header removal

--- templates/single-agent.php
+++ templates/single-agent.php
@@ -8,11 +8,6 @@ get_header(); ?>
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main content" role="main">
 			<?php if ( have_posts() ) : ?>
-				<header class="page-header">
-					<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-					<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
-				</header><!-- .page-header -->
-
 				<?php while ( have_posts() ) : the_post(); ?>
                     <?php echo Realia_Template_Loader::load( 'content-agent' ); ?>
 				<?php endwhile; ?>
