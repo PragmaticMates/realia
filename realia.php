636f3074c2148555250c3e886cf8a0469f62e17c
Lukas <lukas.vinclav@gmail.com>
2015-06-03 16:50:15 +0200

    typo

--- realia.php
+++ realia.php
@@ -12,7 +12,7 @@ License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
-if ( ! class_exists( 'Realia ' ) ) {
+if ( ! class_exists( 'Realia' ) ) {
     /**
      * Class Realia
      *
