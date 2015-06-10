a2e9b26bb6bd54a9bb5d55da0300a9c8189d907b
imlukas <lukas@pragmaticmates.com>
2015-06-10 11:23:06 +0200

    breadcrumb better structure

--- templates/misc/breadcrumb.php
+++ templates/misc/breadcrumb.php
@@ -1,19 +1,33 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-?>
-
 <ol class="breadcrumb">
 	<?php if ( is_front_page() ): ?>
 		<li><?php echo __( 'Home', 'realia' ); ?></li>
 		<li><?php bloginfo( 'name' ); ?></li>
 	<?php elseif ( ! is_home() ) : ?>
         <li><a href="<?php echo site_url(); ?>"><?php echo __( 'Home', 'realia' ); ?></a></li>
+        
+        <?php if ( is_archive() && is_tax() ) : ?>        	
+        	<?php if ( is_tax() ) : ?>
+        		<?php 
+	        	global $wp_query;
+        		$tax = $wp_query->get_queried_object();
+ 				$term = get_term_by( 'slug', get_query_var( 'term' ), $tax->taxonomy );
+				$ancestors = get_ancestors( $term->term_id, $tax->taxonomy  );
+				?>
 
-        <?php if ( is_archive() && is_tax() ) : ?>
-        	<li><?php echo single_cat_title(); ?></li>
-        <?php elseif ( is_archive() ) : ?>
+				<?php if ( is_array( $ancestors ) ) : ?>
+					<?php foreach ( array_reverse( $ancestors ) as $ancestor ) : ?>
+						<li>
+							<?php $term = get_term( $ancestor, $tax->taxonomy ); ?>
+							<a href="<?php echo get_term_link( $term->term_id, $tax->taxonomy); ?>"><?php echo $term->name; ?></a>
+						</li>
+					<?php endforeach; ?>
+				<?php endif; ?>
+				
+				<li><?php echo single_cat_title(); ?></li>				
+        	<?php else: ?>
+	        	<li><?php echo single_cat_title(); ?></li>
+	        <?php endif; ?>
+        <?php elseif ( is_archive() ) : ?>        	
         	<li><?php post_type_archive_title(); ?></li>
         <?php endif; ?>
 
@@ -27,13 +41,13 @@ if ( ! defined( 'ABSPATH' ) ) {
         <?php elseif ( is_page() ): ?>
                 <li><?php the_title(); ?></li>
         <?php endif; ?>
-	<?php elseif ( is_tag() ) : ?>
+	<?php elseif ( is_tag() ) : ?> 		
 		<li><?php single_tag_title() ?></li>
 	<?php elseif ( is_day() ) : ?>
 		<li><?php echo __( 'Archive for', 'realia'); ?>  <?php the_time( 'F jS, Y' ); ?></li>
 	<?php elseif ( is_month() ): ?>
 		<li><?php echo __( 'Archive for', 'realia'); ?>  <?php the_time( 'F, Y' ); ?></li>
-	<?php elseif (is_year()) : ?>
+	<?php elseif (is_year()) : ?> 
 		<li><?php echo __( 'Archive for', 'realia'); ?>  <?php the_time( 'Y' ); ?></li>
 	<?php elseif ( is_author() ): ?>
 		<li><?php echo __( 'Author Archive', 'realia' ); ?></li>
