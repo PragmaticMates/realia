544402348b5c71c2966a02dfdb2ab79bef384e68
Lukas <lukas.vinclav@gmail.com>
2015-06-23 17:07:34 +0200

    syntax error fix

--- templates/misc/profile-form.php
+++ templates/misc/profile-form.php
@@ -10,22 +10,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 <form method="post" action="<?php the_permalink(); ?>" class="change-profile-form">
 	<div class="form-group">
 		<label><?php echo __( 'Nickname', 'realia' ); ?></label>
-		<input type="text" name="nickname" class="form-control" value="<?php echo ! empty( $data->nickname) ? esc_attr( $data->nickname ) ? ''; ?>" required="required">
+		<input type="text" name="nickname" class="form-control" value="<?php echo ! empty( $data->nickname ) ? esc_attr( $data->nickname ) : ''; ?>" required="required">
 	</div><!-- /.form-group -->
 
 	<div class="form-group">
 		<label><?php echo __( 'E-mail', 'realia' ); ?></label>
-		<input type="email" name="email" class="form-control" value="<?php echo ! empty( $data->user_email) ? esc_attr( $data->user_email ) ? ''; ?>"  required="required">
+		<input type="email" name="email" class="form-control" value="<?php echo ! empty( $data->user_email ) ? esc_attr( $data->user_email ) : ''; ?>"  required="required">
 	</div><!-- /.form-group -->
 
 	<div class="form-group">
 		<label><?php echo __( 'First name', 'realia' ); ?></label>
-		<input type="text" name="first_name" class="form-control" value="<?php echo ! empty( $data->first_name) ? esc_attr( $data->first_name ) ? ''; ?>">
+		<input type="text" name="first_name" class="form-control" value="<?php echo ! empty( $data->first_name ) ? esc_attr( $data->first_name ) : ''; ?>">
 	</div><!-- /.form-group -->
 
 	<div class="form-group">
 		<label><?php echo __( 'Last name', 'realia' ); ?></label>
-		<input type="text" name="last_name" class="form-control" value="<?php echo ! empty( $data->last_name) ? esc_attr( $data->last_name ) ? ''; ?>">
+		<input type="text" name="last_name" class="form-control" value="<?php echo ! empty( $data->last_name ) ? esc_attr( $data->last_name ) : ''; ?>">
 	</div><!-- /.form-group -->
 
 	<button type="submit" name="change_profile_form"><?php echo __( 'Change Profile', 'realia' ); ?></button>
