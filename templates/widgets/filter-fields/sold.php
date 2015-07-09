8667805f7339ea3862dbde9f118fc65d1ccc798e
Lukas <lukas.vinclav@gmail.com>
2015-07-09 09:39:26 +0200

    checkbox structure

--- templates/widgets/filter-fields/sold.php
+++ templates/widgets/filter-fields/sold.php
@@ -1,8 +1,9 @@
 <?php if ( empty( $instance['hide_sold'] ) ) : ?>
 	<div class="form-group">
 		<div class="checkbox">
+			<input type="checkbox" name="filter-sold" <?php echo ! empty( $_GET['filter-sold'] ) ? 'checked' : ''; ?> id="<?php echo esc_attr( $args['widget_id'] ); ?>_sold">
+
 			<label for="<?php echo esc_attr( $args['widget_id'] ); ?>_sold">
-				<input type="checkbox" name="filter-sold" <?php echo ! empty( $_GET['filter-sold'] ) ? 'checked' : ''; ?> id="<?php echo esc_attr( $args['widget_id'] ); ?>_sold">
 				<?php echo __( 'Sold', 'realia' ); ?>
 			</label>
 		</div><!-- /.checkbox -->
