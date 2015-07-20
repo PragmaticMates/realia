095c990f2bd52cca39655b4c22bceb342e86f4d6
lukas <lukas@lukass-MacBook-Pro.local>
2015-07-20 15:43:05 +0200

    property taxonomies templates adjustments

--- templates/taxonomy-amenities.php
+++ templates/taxonomy-amenities.php
@@ -14,21 +14,43 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php
-				/**
-				 * realia_before_agency_archive
-				 */
-				do_action( 'realia_before_agency_archive' );
+			/**
+			 * realia_before_property_archive
+			 */
+			do_action( 'realia_before_property_archive' );
 			?>
 
+			<?php if ( get_theme_mod( 'realia_general_show_property_archive_as_grid', null ) == '1' ) : ?>
+				<div class="property-box-archive type-box item-per-row-3">
+					<div class="properties-row">
+			<?php endif; ?>
+
+			<?php $index = 0; ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-                <?php echo Realia_Template_Loader::load( 'agencies/row' ); ?>
+				<?php if ( get_theme_mod( 'realia_general_show_property_archive_as_grid', null ) == '1' ) : ?>
+					<div class="property-container">
+						<?php echo Realia_Template_Loader::load( 'properties/box' ); ?>
+					</div><!-- /.property-container -->
+
+					<?php if ( 0 == ( ( $index + 1 ) % 3 ) && Realia_Query::loop_has_next() ) : ?>
+						</div><div class="properties-row">
+					<?php endif; ?>
+				<?php else : ?>
+					<?php echo Realia_Template_Loader::load( 'properties/row' ); ?>
+				<?php endif; ?>
+				<?php $index++; ?>
 			<?php endwhile; ?>
 
+			<?php if ( get_theme_mod( 'realia_general_show_property_archive_as_grid', null ) == '1' ) : ?>
+					</div><!-- /.properties-row -->
+				</div><!-- /.property-box-archive -->
+			<?php endif; ?>
+
 			<?php
-				/**
-				 * realia_after_agency_archive
-				 */
-				do_action( 'realia_after_agency_archive' );
+			/**
+			 * realia_after_property_archive
+			 */
+			do_action( 'realia_after_property_archive' );
 			?>
 
 			<?php the_posts_pagination( array(
@@ -39,6 +61,7 @@ get_header(); ?>
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
+
 	</main><!-- .site-main -->
 </section><!-- .content-area -->
 
